7e50ae5c81bf434fe4ff77fa24b69e5d4b8236f0
solar05 <artem00298@gmail.com>
2020-09-26 21:15:49 +0300

    Done refactoring

--- lib/inn_checker_service_web/controllers/user_controller.ex
+++ lib/inn_checker_service_web/controllers/user_controller.ex
@@ -1,44 +1,71 @@
 defmodule InnCheckerServiceWeb.UserController do
   use InnCheckerServiceWeb, :controller
 
+  alias InnCheckerServiceWeb.Authentication
+  import InnCheckerService.Accounts.User
+
   def index(conn, _params) do
-    ban_list = GenServer.call(:ban_server, {:ban_list, %{}})
-    render(conn, "index.html", users: ban_list)
+    user = Authentication.current_user(conn)
+
+    if can?(user, "index_users") do
+      ban_list = GenServer.call(:ban_server, {:ban_list, %{}})
+      render(conn, "index.html", users: ban_list)
+    else
+      conn
+      |> put_flash(:error, "Недостаточно прав.")
+      |> redirect(to: Routes.document_path(conn, :index))
+    end
   end
 
   def create(conn, %{"client" => client}) do
-    GenServer.call(:ban_server, {:unban, %{client: client}})
+    user = Authentication.current_user(conn)
 
-    conn
-    |> put_flash(:success, "Пользователь успешно разблокирован!")
-    |> redirect(to: Routes.user_path(conn, :index))
+    if can?(user, "unban_users") do
+      GenServer.call(:ban_server, {:unban, %{client: client}})
+
+      conn
+      |> put_flash(:success, "Пользователь успешно разблокирован!")
+      |> redirect(to: Routes.user_path(conn, :index))
+    else
+      conn
+      |> put_flash(:error, "Недостаточно прав.")
+      |> redirect(to: Routes.document_path(conn, :index))
+    end
   end
 
   def delete(conn, %{"client" => client, "minutes" => minutes}) do
-    if minutes != "" and Regex.match?(~r(^[0-9]+$), minutes) do
-      case GenServer.call(
-             :ban_server,
-             {:ban_user, %{client: client, minutes: String.to_integer(minutes)}}
-           ) do
-        :ok ->
-          conn
-          |> put_flash(:success, "Пользователь успешно заблокирован!")
-          |> redirect(to: Routes.user_path(conn, :index))
+    user = Authentication.current_user(conn)
 
-        :banned ->
-          conn
-          |> put_flash(:info, "Пользователь был заблокирован ранее!")
-          |> redirect(to: Routes.user_path(conn, :index))
+    if can?(user, "ban_users") do
+      if minutes != "" and Regex.match?(~r(^[0-9]+$), minutes) do
+        case GenServer.call(
+               :ban_server,
+               {:ban_user, %{client: client, minutes: String.to_integer(minutes)}}
+             ) do
+          :ok ->
+            conn
+            |> put_flash(:success, "Пользователь успешно заблокирован!")
+            |> redirect(to: Routes.user_path(conn, :index))
 
-        _ ->
-          conn
-          |> put_flash(:error, "Произошла ошибка!")
-          |> redirect(to: Routes.user_path(conn, :index))
+          :banned ->
+            conn
+            |> put_flash(:info, "Пользователь был заблокирован ранее!")
+            |> redirect(to: Routes.user_path(conn, :index))
+
+          _ ->
+            conn
+            |> put_flash(:error, "Произошла ошибка!")
+            |> redirect(to: Routes.user_path(conn, :index))
+        end
+      else
+        conn
+        |> put_flash(:error, "Произошла ошибка!")
+        |> redirect(to: Routes.user_path(conn, :index))
       end
     else
       conn
-      |> put_flash(:error, "Произошла ошибка!")
-      |> redirect(to: Routes.user_path(conn, :index))
+      |> put_flash(:error, "Недостаточно прав.")
+      |> redirect(to: Routes.document_path(conn, :index))
     end
   end
 end
